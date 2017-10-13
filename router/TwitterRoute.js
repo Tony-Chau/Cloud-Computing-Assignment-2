bc640c563723fb8df7c762bd99d52cc42d03e898
Tony Chau <tonychau923@gmail.com>
2017-10-13 20:21:28 +1000

    made some changes

--- router/TwitterRoute.js
+++ router/TwitterRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const MongoServer = require('./functions/mongodb.js');
 
 router.get('/', function(req, res){
     res.render('index', {
