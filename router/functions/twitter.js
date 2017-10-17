7c759beddce609f984eef1ec5979cd6497f4b319
Tony Chau <tonychau923@gmail.com>
2017-10-17 22:31:08 +1000

    twitter function now functional and can obtain data, but now we need to code it differently due to these new changes

--- router/functions/twitter.js
+++ router/functions/twitter.js
@@ -1,19 +1,23 @@
-const Twitter = require('twitter');
+const Twit = require('twit');
+var twitter = new Twit({
+  consumer_key: 'Qoj9WAdgTGtPwb48VYSqdBEEF',
+  consumer_secret: 'qkmWJtFTH08OYfPPEg3cX2XYiKmv3fCFj29c3P1b4Y34ICZ2Xf',
+  access_token: '901995605249056768-GvylBUk69pnwaTHk57bfZiVSTuNBLlC',
+  access_token_secret: 'bToRXg7TR7MylPex9kq3t3uJNgHGSeWMh8R5PAUH6FuLn'
+});
 
-var client = new Twitter({
-    consumer_key: 'Qoj9WAdgTGtPwb48VYSqdBEEF',
-    consumer_secret: 'qkmWJtFTH08OYfPPEg3cX2XYiKmv3fCFj29c3P1b4Y34ICZ2Xf',
-    access_token_key: '901995605249056768-GvylBUk69pnwaTHk57bfZiVSTuNBLlC',
-    access_token_secret: 'bToRXg7TR7MylPex9kq3t3uJNgHGSeWMh8R5PAUH6FuLn'
-  });
 
-//   client.get('search/tweets', {q: '#'}, function(error, tweets, response) {
-//     console.log(tweets);
-//     console.log(response);
-//  });
 
- client.get('search/tweets', {q: '%40twitterapi', lang: 'en', count: 1000}, function(error, tweets, response) {
-    console.log(tweets);
-  });
-
-  
+module.exports = {
+  search: function (req, res){
+    //var query = req.query.search;
+    var query = '#' + query; //(or %20)
+    twitter.get('search/tweets', { q: query }, function(err, data, response) {
+      var TwitterData = JSON.Parse(data);
+      res.render('index', {
+        Title: 'Twitter Hashtag Search',
+        data: data
+      });
+    });
+  }
+}
--- router/mainroute.js
+++ router/mainroute.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const MongoServer = require('./functions/mongodb.js');
-
+const twitter = require('./functions/twitter.js');
 
 router.get('/', function(req, res){
-    res.render('index', {
-        Title: 'Twitter Hashtag Search'
-    });
+    twitter.search(req, res);
 });
 
 module.exports = router;
