123fc63257caf81caa7ce5272ba92289b1dd2a18
Tony Chau <tonychau923@gmail.com>
2017-10-18 23:01:43 +1000

    included the mysql package in package.json and will complete the code tomrrow, but will be thoughtful to complete the front-end of the code

--- router/functions/mysql.js
+++ router/functions/mysql.js
@@ -1,7 +1,22 @@
 //Allan, make sure u include these details to ur mysql
+const mysql = require('mysql');
 const mysqlConnection = {
     dbHost: 'twitterstreamcloudcomputing.heliohost.org',
     dbDatabase: 'tonychau_twitterstream',
     dbUser: 'tonychau_manager',
     dbPassword: 'twitter'
 }
+var con = mysql.createConnection({
+    host: mysqlConnection.dbHost,
+    user: mysqlConnection.dbUser,
+    password: mysqlConnection.dbPassword
+});
+
+//Connection Code
+// con.connect(function(err){
+//     if (err){
+//         throw err;
+//     }else{
+//         console.log('Connected');
+//     }
+// });
