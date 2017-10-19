27da5c2653b530a8e016b7de459e4f5ba705be21
Tony Chau <tonychau923@gmail.com>
2017-10-19 12:14:38 +1000

    implemented more functions for mysql

--- router/functions/mysql.js
+++ router/functions/mysql.js
@@ -12,11 +12,52 @@ var con = mysql.createConnection({
     password: mysqlConnection.dbPassword
 });
 
-//Connection Code
-// con.connect(function(err){
-//     if (err){
-//         throw err;
-//     }else{
-//         console.log('Connected');
-//     }
-// });
+module.exports = {
+    InsertTwitterTable: function (author, text, mentions, length){
+        con.connect(function(err){
+            if (err){
+                throw err;
+            }else{
+                var sql = '';
+                for (var i = 0; i < length - 1; i += 1){
+                    sql += 'INSERT INTO TwitterTable(author, text, mentions) VALUES (';
+                    sql += "'"+ author[i]+ "', '" + text[i] + "', '" + mentions + "');";
+                }
+                con.query(sql, function(err){
+                    if (err){
+                        throw err;
+                    }
+                });
+            }
+        });
+    }, 
+    ResetTwitterTable: function(){
+        con.connect(function(err){
+            if (err){
+                throw err;
+            }else{
+                var sql = 'TURNUCATE TwitterTable;';
+                con.query(sql, function(err){
+                    if (err){
+                        throw err;
+                    }
+                });
+            }
+        });
+    },
+    SelectTwitterTable : function(req, res, search){
+        con.connect(function(err){
+            if (err){
+                throw err;
+            }else{
+                var sql = 'Select * FROM TwitterTable;';
+                con.query(sql, function(err, result){
+                    if (err){
+                        throw err;
+                    }
+                    //do stuff here
+                });
+            }
+        });
+    }
+}
