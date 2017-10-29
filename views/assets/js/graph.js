77fe93f1dd153680d165dd173524a3c19a2ff66b
Tony Chau <tonychau923@gmail.com>
2017-10-29 16:55:01 +1000

    here is the plot.ly code

--- views/assets/js/graph.js
+++ views/assets/js/graph.js
@@ -1,18 +1,8 @@
-// var data = [
-//   {
-//     x: ['giraffes', 'orangutans', 'monkeys'],
-//     y: [20, 14, 23],
-//     type: 'bar'
-//   }
-// ];
-//
-// Plotly.newPlot('myDiv', data);
-// var plotly = require('plotly')("allan1q2w", "Ego72h93Mkz4ACfZ20uC")
-
-var data = [{x:[0,1,2], y:[3,2,1], type: 'bar'}];
-var layout = {fileopt : "overwrite", filename : "simple-node-example"};
-
-plotly.plot(data, layout, function (err, msg) {
-	if (err) return console.log(err);
-	console.log('hello');
-});
+var TESTER = document.getElementById('myDiv');
+Plotly.plot( TESTER, [{
+x: [1, 2, 3, 4, 5],
+y: [1, 2, 4, 8, 16] }], 
+{
+margin: {
+	 t: 0 
+	} } );
