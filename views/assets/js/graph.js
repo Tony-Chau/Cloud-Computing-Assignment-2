74200792c7040c991df758f23a2effab3e6ebbbb
Allan <allan1q2w@gmail.com>
2017-10-31 00:37:28 +1000

    table

--- views/assets/js/graph.js
+++ views/assets/js/graph.js
@@ -7,25 +7,50 @@ onload = function getData(){
 function createGraph(data){
   var graphObject = document.getElementById('graphDiv');
   var twitterGraph = {
-    x: topHash,
-    y: topPoint,
+    x: data.topHash,
+    y: data.topPoint,
     name: 'Top 10 Hashtag Search',
     type: 'bar'
   };
-  
-  
+
   var data = [twitterGraph];
-  
-  var layout = {barmode: 'group', width: (window.innerWidth/1.3), height: (window.innerHeight/1.4)};
-  
+  var layout = {barmode: 'group', width: (window.innerWidth/1.3), height: (window.innerHeight/1.3)};
+
   Plotly.newPlot(graphObject, data, layout);
+  $('#twitterDiv').html(twitterTable(data));
+}
+
+function twitterTable(data){
+  var graphObject = document.getElementById('twitterDiv');
+  alert(data.topHash);
+  var s = "";
+  s = '<table class="table table-boardered">';
+  s += '<thread>';
+  s += '<tr>';
+  s += '<th>Ranking</th>';
+  s += '<th>Top Hashtag</th>';
+  s += '<th>Hastage Frequency</th>';
+  s += '</tr>';
+  s += '</thread>';
+  s += '<tbody>';
+
+  for (var i=0; i < 10; i++){
+    s += '<tr>';
+    s += '<td>' + (i+1) + '</td>';
+    s += '<td>' + data.topHash[i] + '</td>';
+    s += '<td>' + data.topPoint[i] + '</td>';
+    s += '</tr>';
+  }
+  s += '</tbody>';
+  s += '</table>';
+  return s
 }
 
 
-function getSearch(send){
-  var topHash= x[i];
-  var topPoint= y[i];
-}
+// function getSearch(send){
+//   var topHash= x[i];
+//   var topPoint= y[i];
+// }
 
 // var twitterGraph = {
 //   x: ['giraffes', 'orangutans', 'monkeys', 'a','b','c','d','e','f', 'g'],
@@ -33,5 +58,3 @@ function getSearch(send){
 //   name: 'Top 10 Hashtag Search',
 //   type: 'bar'
 // };
-
-
