001c64943814d6e18b267f6d0353042113aee619
Tony Chau <tonychau923@gmail.com>
2017-11-01 14:14:09 +1000

    this should wreck it

--- router/functions/mysql.js
+++ router/functions/mysql.js
@@ -109,11 +109,15 @@ module.exports = {
                         }
                         point[i] += increment;
                       }
-                      for (var i = 0; i < result.length; i += 1){
-                          for(var j = 0; j < array[i].length; j += 1){
-                              var k = array[i][j];
-                          }
-                      }
+                      setInterval(function (){
+                        var counter = 0;
+                        for (var i = 0; i < result.length; i += 1){
+                            for(var j = 0; j < array[i].length; j += 1){
+                                counter += 1;
+                            }
+                        }
+                      }, 100);
+
                       //orders the array
                       var rank = tool.Ranking(entry, point);
                       //Find the top 10 search, but if less than varies depending on the result length
