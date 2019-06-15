cd60ea7f3a50ee993edad14992f69e9ec2b5cd66
Tony Chau <tonychau923@gmail.com>
2019-06-15 19:19:55 +1000

    updated this file for a reason

--- router/functions/twitter.js
+++ router/functions/twitter.js
@@ -1,9 +1,9 @@
 const Twit = require('twit');
 var twitter = new Twit({
-  consumer_key: 'Qoj9WAdgTGtPwb48VYSqdBEEF',
-  consumer_secret: 'qkmWJtFTH08OYfPPEg3cX2XYiKmv3fCFj29c3P1b4Y34ICZ2Xf',
-  access_token: '901995605249056768-GvylBUk69pnwaTHk57bfZiVSTuNBLlC',
-  access_token_secret: 'bToRXg7TR7MylPex9kq3t3uJNgHGSeWMh8R5PAUH6FuLn'
+  consumer_key: 'Cannot find it',
+  consumer_secret: 'Its a secret',
+  access_token: 'Whant to know the access token?',
+  access_token_secret: 'Haha. Never'
 });
 const tool = require('./tools.js'); 
 const mysql = require('./mysql.js');
